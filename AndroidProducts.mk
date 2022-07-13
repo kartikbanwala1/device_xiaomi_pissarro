9fcb06ff99164ff9d1997ce4e055b80a1469231f
kartikbanwala1 <83795028+kartikbanwala1@users.noreply.github.com>
2022-07-13 20:32:44 +0530

    Update AndroidProducts.mk
    Signed-off-by: kartikbanwala1 <83795028+kartikbanwala1@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pissarro.mk
+    $(LOCAL_DIR)/cherish_pissarro.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pissarro-user \
-    lineage_pissarro-userdebug \
-    lineage_pissarro-eng
+    cherish_pissarro-user \
+    cherish_pissarro-userdebug \
+    cherish_pissarro-eng
